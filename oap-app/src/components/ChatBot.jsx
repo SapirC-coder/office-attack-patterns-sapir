07fee390f478f899adc57202f7db5c1ccc610f21
SapirC <sapir.co10@gmail.com>
2021-04-12 01:52:08 +0300

    trying bots

--- oap-app/src/components/ChatBot.jsx
+++ oap-app/src/components/ChatBot.jsx
@@ -7,16 +7,56 @@ function CustomChatbot(props) {
       floating: true
     };
     const steps = [
-       {
-        id: "Greet",
-        message: "Hello",
-        trigger: "Done"
-       },
-       {
-        id: "Done",
-        message: "Have a great day !!",
-        end: true
-       }
+      {
+        id: "start",
+        message: "Hey! I'm here to help you to search about some more data! would you like to search in VirusTotal or in our Database?",
+        trigger: "search_choice",
+      },
+      {
+        id: "search_choice",
+        user: true,
+        trigger: (value) => {return value.toString().toLowerCase().includes("virustotal")? "virustotal_choice" : value.toString().toLowerCase().includes("database")? "database_choice" : "start"},
+      },
+      {
+        id: "database_choice",
+        message: "You chose to search in database! get names of attack patterns that include:",
+        trigger: "options_database",
+      },
+      {
+        id: 'options_database',
+        options: [
+          { value: 1, label: 'mitre platform', trigger: 'mitre_plat_input' },
+          { value: 2, label: 'phase name', trigger: 'phase_name_input' },
+          { value: 3, label: 'part of name', trigger: 'part_name_input' },
+        ],
+      },
+      {
+        id: "mitre_plat_input",
+        user: true,
+        trigger: "handle_db_choice"
+      },
+      {
+        id: "phase_name_input",
+        user: true,
+        trigger: "handle_db_choice"
+      },
+      {
+        id: "part_name_input",
+        user: true,
+        trigger: "handle_db_choice"
+      },
+      {
+        id: "handle_db_choice",
+        user: true,
+        trigger: "start"
+      },
+      {
+        id: "virustotal_choice",
+        message: "You chose to search in VirusTotal!",
+        trigger: "start"
+      },
+      
+    
      ];
     
     return <ChatBot steps={steps} {...config} />;
